e6306f08e75277b502ae36ade387f99b88da38d8
pr39402 <pr39402@zut.edu.pl>
2021-06-13 13:30:25 +0200

    multiclass classification

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from sklearn import metrics
 import math
 import pickle
 from pathlib import Path
+import time
 
 
 def spectral_centroid_feature(x, sr, draw=False):
@@ -30,7 +31,7 @@ def normalize(x, axis=0):
 
 
 def mfcc_feature(x, sr, draw=False):
-    mfccs = librosa.feature.mfcc(x, sr=sr)
+    mfccs = librosa.feature.mfcc(x, sr=sr, n_mfcc=100000)
     if draw:
         librosa.display.specshow(mfccs, sr=sr, x_axis='time')
         plt.grid()
@@ -58,7 +59,7 @@ def prepare_data(method='mfcc'):
     x_data = []
     y_data = []
 
-    for root, dirs, files in os.walk("./dataset/", topdown=False):
+    for root, dirs, files in os.walk("./dataset_multiclass/", topdown=False):
         for name in files:
             path = os.path.join(root, name)
 
@@ -67,7 +68,7 @@ def prepare_data(method='mfcc'):
                 x_val = mfcc_feature(x, sr, False)
                 x_val = x_val.flatten()
                 x_val = x_val.tolist()
-                x_val = x_val[:25800]
+                x_val = x_val[:100000]
             elif method == 'fft':
                 x_val = fft_feature(x, sr, draw=False)
                 x_val = x_val.flatten()
@@ -82,12 +83,15 @@ def prepare_data(method='mfcc'):
                 x_val = x_val.flatten()
                 x_val = x_val.tolist()
                 x_val = x_val[:1290]
-
             x_data.append(x_val)
-            if 'mix' in path:
-                y_data.append(-1)
-            else:
+            if 'rock' in path:
                 y_data.append(1)
+            if 'hiphop' in path:
+                y_data.append(2)
+            if 'pop' in path:
+                y_data.append(3)
+            if 'metal' in path:
+                y_data.append(4)
     return x_data, y_data
 
 
@@ -97,6 +101,7 @@ def run_tests(methods = [], save = False, pickles_filepaths = []):
         return
 
     for i, m in enumerate(methods):
+        start = time.time()
         X, y = prepare_data(m)
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 
@@ -116,21 +121,54 @@ def run_tests(methods = [], save = False, pickles_filepaths = []):
             pickle.dump(clf, opened_file)
 
         y_pred = clf.predict(X_test)
+        stop = time.time()
+        f = open(m + "_feature_acc.txt", "w")
+        f.write("y_test | y_pred\n")
+        for j in range(len(y_test)):
+            f.write(str(y_test[j]) + " | " + str(y_pred[j]) + "\n")
+        f.close()
+
         res = metrics.accuracy_score(y_test, y_pred) * 100
         res = "{:.2f}".format(res)
+        print("------------------------------------------------")
         print(f"SVM Accuracy with {m} feature: {res}%.")
+        multiclass_accuracy(y_test, y_pred)
+        print("Elapsed time: " + str(stop - start) + "s")
+        print("------------------------------------------------")
+
+
+def multiclass_accuracy(y_test, y_pred):
+    sum1 = 0
+    sum2 = 0
+    sum3 = 0
+    sum4 = 0
+    for i in range(len(y_test)):
+        if y_pred[i] == y_test[i] and y_test[i] == 1:
+            sum1 += 1
+        if y_pred[i] == y_test[i] and y_test[i] == 2:
+            sum2 += 1
+        if y_pred[i] == y_test[i] and y_test[i] == 3:
+            sum3 += 1
+        if y_pred[i] == y_test[i] and y_test[i] == 4:
+            sum4 += 1
+    acc1 = (sum1 / y_test.count(1)) * 100
+    acc2 = (sum2 / y_test.count(2)) * 100
+    acc3 = (sum3 / y_test.count(3)) * 100
+    acc4 = (sum4 / y_test.count(4)) * 100
+    acc1 = "{:.2f}".format(acc1)
+    acc2 = "{:.2f}".format(acc2)
+    acc3 = "{:.2f}".format(acc3)
+    acc4 = "{:.2f}".format(acc4)
+
+    print("Accuracy for Rock: " + str(acc1) + "%")
+    print("Accuracy for Hiphop: " + str(acc2) + "%")
+    print("Accuracy for Pop: " + str(acc3) + "%")
+    print("Accuracy for Metal: " + str(acc4) + "%")
+
+
 
 if __name__ == '__main__':
     methods = ['mfcc', 'fft', 'spectralCentroid']
-
-    audio_path = 'dataset/rock/rock.00000.wav'
-    x, sr = librosa.load(audio_path)
-    sc = spectral_centroid_feature(x, sr, draw=False)
-    # print(f'sc = {sc}')
-    mfcc = mfcc_feature(x, sr, False)
-    # print(f'mfcc = {mfcc}')
-    fft_val = fft_feature(x, sr, draw=False)
-    # print(f'fft_val = {fft_val}')
 
     # Run clearly
     # run_tests(methods, True)
